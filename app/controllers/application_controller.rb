5a33933485f040d9f6c52d9a4e608e90b19d21dc
root <root@ubuntu-tutum.kl.op>
2015-10-10 12:05:50 -0400

    my first sample-apps by jerry

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def hello
+    require 'socket'
+    ip = `/sbin/ifconfig eth0 | grep 'net addr' | awk '{print $2}' | sed -e s/.*://`.strip
+    render text: "Hello World   \n IP is "+ip
+  end 
+
+end
+
